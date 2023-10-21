2d41de60304c2304d027d3356126e7698d7f4c84
Prakhar Verma <prakharverma1022@gmail.com>
2023-10-21 06:31:10 +0530

    Code for 01_variables.js is done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "prakharverma1022@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "972ke2"
+accountCity = "New Delhi"
+
+// accountId = 2 // not allowed
+
+// console.log(accountId);
+
+/* 
+Prefer not to use var
+because of block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
