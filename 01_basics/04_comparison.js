f221406c9fc602c8efed9fb415e904d21de90e4b
Prakhar Verma <prakharverma1022@gmail.com>
2023-10-21 07:17:24 +0530

    Code for 04_comparison.js is done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(1 < 5);
+// console.log(5 <= 5);
+// console.log(6 > 5);
+// console.log(1 >= 5);
+// console.log(1 != 5);
+// console.log(1 == 5);
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+
+// The reason is that an equality check == and conparisons > < >= <= works differently
+// In case of comparison operators null gets change to zero
+// console.log(null == 0) // false
+// console.log(null < 0) // false
+// console.log(null >= 0) // true
+
+// no conversion is there in undefined
+// console.log(undefined == 0) // false
+// console.log(undefined < 0) // false
+// console.log(undefined >= 0) // true
+
+
+// == -> only checks the value
+// === -> checks the value as well as data type
