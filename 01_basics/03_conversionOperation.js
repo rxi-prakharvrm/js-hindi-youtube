fd7f5d48a4033719346a370cbd67323c990dcb3f
Prakhar Verma <prakharverma1022@gmail.com>
2023-10-21 07:01:21 +0530

    Add operations code in conversion operation program

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,5 +29,39 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+// ************************************************* Operations **************************************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Hitesh"
+
+// console.log(str1 + str2)
+// console.log("1" + 2) // 12
+// console.log(1 + "2") // 12
+// console.log("1" + 2 + 3) // 123
+// console.log(1 + 2 + "3") // 33
+// console.log("1" + 2 + "3") // 123
+// console.log(1 + "2" + 3) // 123
+
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 0
+gameCounter++;
+console.log(gameCounter);
