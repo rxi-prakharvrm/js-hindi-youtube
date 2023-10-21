e3a8eab02be70c09e5cab417c8b2dd73ad29f564
Prakhar Verma <prakharverma1022@gmail.com>
2023-10-21 06:31:36 +0530

    Code for 02_dataTypes.js is done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as new version
+
+// alert("Hello") // It is a function of browser and we are using nodejs
+
+// console.log(3
+//     +
+//     3)  // Code readability should be high
+
+// console.log("Hello India")
+
+let name = "Prakhar"
+let age = 22
+let isLoggedIn = true
+
+// number => 2^53
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value => empty value (kuch nahi matlab kuch nahi)
+// undefined => variable defined but not initialized (default initializer)
+// symbol => unique
+
+// object
+
+console.log(typeof(name)) // string
+console.log(typeof(age)) // number
+console.log(typeof(isLoggedIn)) // boolean
+console.log(typeof(null)) // object
+console.log(typeof(undefined)) // undefined
+console.log(typeof(NaN)) // number
