6ee316655d96451aecfe7ad9f38e0bc2aa721fca
Prakhar Verma <prakharverma1022@gmail.com>
2023-10-21 06:51:12 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = undefined
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+let valueInNumber = Number(score) // string to number conversion
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// "33" => 33 => number
+// "33abc" => NaN => number
+// true => 1
+// false => 0
+// undefined => NaN
+
+let isLoggedIn = " "
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// >1 => true
+// 0 => false
+// "" => false
+// " " => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof(stringNumber))
